4915abc80da84753afae748d9603ecc3d0410dd5
vixelambda <dir.victor@yandex.ru>
2022-05-02 15:26:04 +0300

    Create spring boot app

---
+++ src/test/java/com/voyagetourcompany/VoyageTourCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voyagetourcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageTourCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
